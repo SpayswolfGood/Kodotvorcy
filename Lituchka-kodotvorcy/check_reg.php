991f18aa3243a3c543abd6f25bf05694b0a29a67
1hihik1 <123875726+1hihik1@users.noreply.github.com>
2024-05-13 11:53:30 +0400

    Добавил обработчик формы для авторизации
    Добавил обработчик формы для авторизации в login.php

---
+++ Lituchka-kodotvorcy/check_reg.php
@@ -0,0 +1,52 @@
+<?php
+session_start();
+
+if (isset($_POST['user_login'], $_POST['password'])) {
+    $user_login = htmlspecialchars(trim($_POST['user_login']));
+    $password = htmlspecialchars(trim($_POST['password']));
+    if (empty($user_login) || empty($password)) {
+        exit("Вы ввели не всю информацию, вернитесь назад и заполните все поля!");
+    }
+    
+    include("db_connect.php");
+
+    $stmt = $db->prepare("SELECT * FROM users WHERE user_login = ?");
+    $stmt->bind_param("s", $user_login);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $myrow = $result->fetch_assoc();
+
+    if (empty($myrow['password'])) {
+        exit("Извините, введенные вами логин или пароль неверны.");
+    } else {
+        if ($user_login == 'root' && $password == 'uM6OIzc5KL') {
+            $_SESSION['admin'] = true;
+            $_SESSION['user_login'] = $myrow['user_login'];
+            $_SESSION['id'] = $myrow['id'];
+            header('Location: https://site.local/adminpanel.php');
+        } else {
+            if ($password == $myrow['password']) {
+                $_SESSION['user_login'] = $myrow['user_login'];
+                $_SESSION['id'] = $myrow['id'];
+                $_SESSION['std_group'] = $myrow['std_group'];
+                
+                if ($myrow['role'] == 'Преподаватель') {
+                    header('Location: https://site.local/teacherpanel.php');
+                } else {
+                    header('Location: https://site.local');
+                }
+
+            } else {
+                exit("Извините, введенный вами логин или пароль неверный.");
+            }
+        }
+    }
+
+
+    $stmt->close();
+    $db->close();
+} else {
+    exit("Ошибка: Не удалось получить все необходимые данные.");
+}
+?>
+
